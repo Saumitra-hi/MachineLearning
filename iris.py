eb35eb8f654ce7e5ad98482758645cf933130385
Saumitra-hi <85758638+Saumitra-hi@users.noreply.github.com>
2021-07-14 13:29:20 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -45,3 +45,10 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
+
+#K-CROSS VALIDATION
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {.2f} %".format(accuracies.mean()*100))
+rint("Standard Deviation: {.2f} %".format(accuracies.std()*100))
